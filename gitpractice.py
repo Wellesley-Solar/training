9f11d8f5c6677010cd7100986ca7906400dbe06c
Becky B <rbelisle@wellesley.edu>
2020-06-15 09:30:56 -0400

    Initialing git practice

---
+++ gitpractice.py
@@ -0,0 +1,10 @@
+#%%
+#practice pulling, commiting, and pushing to our git repository using this program
+
+#Add your name to the list of editors 
+editors = ['Becky']
+#increase number of edits by one
+numberofedits = 0
+print('Most recent edit by:', editors[numberofedits])
+
+# %%
