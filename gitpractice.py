7f351c35e5a04d72984a9d7b2e4f9c6a0aff9823
Becky B <rbelisle@wellesley.edu>
2020-06-17 10:31:29 -0400

    edited list such that it has two elements

--- gitpractice.py
+++ gitpractice.py
@@ -2,7 +2,7 @@
 #practice pulling, commiting, and pushing to our git repository using this program
 
 #Add your name to the list of editors 
-editors = ['Becky, Gabe']
+editors = ['Becky', 'Gabe']
 #increase number of edits by one
 numberofedits = 1
 print('Most recent edit by:', editors[numberofedits])
