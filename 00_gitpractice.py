717572fe8011e91dc638538dcad422fd6ba3727b
Becky B <rbelisle@wellesley.edu>
2020-12-02 10:45:31 -0500

    Update 00_gitpractice.py
    Becky training Helena and Amy on using git

--- 00_gitpractice.py
+++ 00_gitpractice.py
@@ -2,9 +2,9 @@
 #practice pulling, commiting, and pushing to our git repository using this program
 
 #Add your name to the list of editors 
-editors = ['Becky', 'Gabe','Annie']
+editors = ['Becky', 'Gabe','Annie', 'Becky']
 #increase number of edits by one
-numberofedits = 1
+numberofedits = 3
 print('Most recent edit by:', editors[numberofedits])
 
 # %%
