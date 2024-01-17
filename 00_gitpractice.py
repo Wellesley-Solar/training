0b3ac03239290b6a85b89ccf80a88c9f9754ad62
Becky B <rbelisle@wellesley.edu>
2024-01-17 15:15:13 -0500

    I added my name!

--- 00_gitpractice.py
+++ 00_gitpractice.py
@@ -2,9 +2,9 @@
 #practice pulling, commiting, and pushing to our git repository using this program
 
 #Add your name to the list of editors 
-editors = ['Becky', 'Gabe','Annie', 'Becky']
+editors = ['Becky','Becky', 'Gabe','Annie', 'Becky']
 #increase number of edits by one
-numberofedits = 3
+numberofedits = 4
 print('Most recent edit by:', editors[numberofedits])
 
 # %%
