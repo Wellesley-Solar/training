d71cfe4528871e169ec20b89d371a9485931c0d4
christinehao7 <113390193+christinehao7@users.noreply.github.com>
2024-01-17 15:23:04 -0500

    Update 00_gitpractice.py

--- 00_gitpractice.py
+++ 00_gitpractice.py
@@ -2,9 +2,9 @@
 #practice pulling, commiting, and pushing to our git repository using this program
 
 #Add your name to the list of editors
-editors = ['Becky', 'Gabe','Annie', 'Becky', 'Luiza']
+editors = ['Becky', 'Gabe','Annie', 'Becky', 'Luiza', 'Christine']
 #increase number of edits by one
-numberofedits = 4
+numberofedits = 5
 print('Most recent edit by:', editors[numberofedits])
 
 # %%
